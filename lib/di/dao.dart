29dff6c3b73b27acb9b67200ad78bed15aceeb79
Rax-x <sianodionisio.rax@gmail.com>
2025-12-10 21:47:41 +0100

    Aggiunge reportDaoProvider

--- lib/di/dao.dart
+++ lib/di/dao.dart
@@ -1,4 +1,5 @@
 import 'package:resqpet/dao/annuncio_dao.dart';
+import 'package:resqpet/dao/report_dao.dart';
 import 'package:resqpet/dao/utente_dao.dart';
 import 'package:resqpet/di/firebase.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -15,4 +16,10 @@ UtenteDao utenteDao(Ref ref) {
 AnnuncioDao annuncioDao(Ref ref) {
   final firebaseFirestore = ref.read(firebaseFirestoreProvider);
   return AnnuncioDao(firebaseFirestore);
+}
+
+@riverpod
+ReportDao reportDao(Ref ref) {
+  final firebaseFirestore = ref.read(firebaseFirestoreProvider);
+  return ReportDao(firebaseFirestore);
 }
