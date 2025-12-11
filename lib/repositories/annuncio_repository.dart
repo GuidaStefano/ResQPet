082122c0f38f6772e95c884e15875895f447695b
ctesta17-hub <c.testa17@studenti.unisa.it>
2025-12-11 14:34:35 +0100

    Refactor repository

--- lib/repositories/annuncio_repository.dart
+++ lib/repositories/annuncio_repository.dart
@@ -3,86 +3,91 @@ import 'package:resqpet/models/annuncio/annuncio.dart';
 import 'package:resqpet/models/annuncio/stato_annuncio.dart';
 import 'package:resqpet/models/annuncio/tipo_annuncio.dart';
 
-class AnnuncioRepository{
-  final AnnuncioDao _annuncioDao;
+class AnnuncioRepository {
+  final AnnuncioDao annuncioDao;
 
-  AnnuncioRepository(this._annuncioDao);
+  AnnuncioRepository({
+    required this.annuncioDao
+  });
 
   Future<List<Annuncio>> getAnnunciVendita() async {
-    return await _annuncioDao.findActiveByTipo(TipoAnnuncio.vendita);
+    return await annuncioDao.findActiveByTipo(TipoAnnuncio.vendita);
   }
 
   Stream<List<Annuncio>> getAnnunciVenditaStream() {
-    return _annuncioDao.findByTipoStream(TipoAnnuncio.vendita);
+    return getAnnunciByStatoAndTipoStream(StatoAnnuncio.attivo, TipoAnnuncio.vendita);
   }
 
   Future<List<Annuncio>> getAnnunciAdozione() async {
-    return await _annuncioDao.findActiveByTipo(TipoAnnuncio.adozione);
+    return await annuncioDao.findActiveByTipo(TipoAnnuncio.adozione);
   }
 
   Stream<List<Annuncio>> getAnnunciAdozioneStream() {
-    return _annuncioDao.findByTipoStream(TipoAnnuncio.adozione);
+    return getAnnunciByStatoAndTipoStream(StatoAnnuncio.attivo, TipoAnnuncio.adozione);
   }
 
   Future<List<Annuncio>> getAnnunciByStatoAndTipo(StatoAnnuncio stato, TipoAnnuncio tipo) async {
-    return await _annuncioDao.findByStatoAndTipo(stato, tipo);
+    return await annuncioDao.findByStatoAndTipo(stato, tipo);
   }
   
   Stream<List<Annuncio>> getAnnunciByStatoAndTipoStream(StatoAnnuncio stato, TipoAnnuncio tipo) {
-    return _annuncioDao.findByStatoAndTipoStream(stato, tipo);
+    return annuncioDao.findByStatoAndTipoStream(stato, tipo);
   }
 
   Future<List<Annuncio>> getAnnunciByCreatore(String creatoreRef) async {
-    return await _annuncioDao.findByCreatore(creatoreRef);
+    return await annuncioDao.findByCreatore(creatoreRef);
   }
 
   Stream<List<Annuncio>> getAnnunciByCreatoreStream(String creatoreRef) {
-    return _annuncioDao.findByCreatoreStream(creatoreRef);
+    return annuncioDao.findByCreatoreStream(creatoreRef);
   }
 
   Future<List<Annuncio>> getAnnunciBySpecie(String specie) async {
-    return await _annuncioDao.findBySpecie(specie);
+    return await annuncioDao.findBySpecie(specie);
   }
 
   Stream<List<Annuncio>> getAnnunciBySpecieStream(String specie) {
-    return _annuncioDao.findBySpecieStream(specie);
+    return annuncioDao.findBySpecieStream(specie);
   }
 
   Future<List<Annuncio>> getAnnunciByRazza(String razza) async {
-    return await _annuncioDao.findByRazza(razza);
+    return await annuncioDao.findByRazza(razza);
   }
 
   Stream<List<Annuncio>> getAnnunciByRazzaStream(String razza) {
-    return _annuncioDao.findByRazzaStream(razza);
+    return annuncioDao.findByRazzaStream(razza);
   }
 
   Future<List<Annuncio>> getAnnunciBySesso(String sesso) async {
-    return await _annuncioDao.findByRazza(sesso);
+    return await annuncioDao.findByRazza(sesso);
   }
 
   Stream<List<Annuncio>> getAnnunciBySessoStream(String sesso) {
-    return _annuncioDao.findByRazzaStream(sesso);
+    return annuncioDao.findByRazzaStream(sesso);
   }
 
   Future<Annuncio> creaAnnuncio(Annuncio annuncio) async {
-    return await _annuncioDao.create(annuncio);
+    return await annuncioDao.create(annuncio);
   }
 
-  Future<Annuncio> finalizzaAnnuncio(String annuncioId) async {
-    final Annuncio? annuncio = await _annuncioDao.findById(annuncioId);
+  Future<void> finalizzaAnnuncio(String annuncioId) async {
+    final Annuncio? annuncio = await annuncioDao.findById(annuncioId);
 
     if(annuncio == null) {
       throw StateError("Annuncio non esistente");
     }
 
-    return annuncio.copyWith(statoAnnuncio: StatoAnnuncio.concluso);
+    await annuncioDao.updateStato(
+      annuncioId,
+      StatoAnnuncio.concluso
+    );
   }
 
   Future<bool> cancellaAnnuncio(String annuncioId) async {
-    return await _annuncioDao.deleteById(annuncioId);
+    return await annuncioDao.deleteById(annuncioId);
   }
 
   Future<Annuncio> aggiornaAnnuncio(Annuncio annuncio) async {
-    return await _annuncioDao.update(annuncio);
+    return await annuncioDao.update(annuncio);
   }
 }
