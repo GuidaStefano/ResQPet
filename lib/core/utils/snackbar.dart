872851610d91542bf3daaa47cea836f145aafb72
alexgf04 <alessioforgione42@gmail.com>
2025-12-18 00:13:17 +0100

    Aggiunte utility per la manipolazione delle snackbar

---
+++ lib/core/utils/snackbar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'theme.dart' show colorSchemeOf;
+
+void _showSnackBar(BuildContext context, String message, {bool error = false}){
+  final colorScheme = colorSchemeOf(context);
+
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      padding: const EdgeInsets.all(10),
+      elevation: 2,
+      showCloseIcon: true,
+      closeIconColor: error ? colorScheme.onError : null,
+      backgroundColor: error ? colorScheme.error : null,
+      content: Text(
+        message, 
+        style: TextStyle(
+          color: error ? colorScheme.onError : null
+        )
+      )
+    )
+  );
+}
+
+void showErrorSnackBar(BuildContext context, String message){
+  _showSnackBar(context, message, error: true);
+}
+
+void showSnackBar(BuildContext context, String message){
+  _showSnackBar(context, message);
+}
