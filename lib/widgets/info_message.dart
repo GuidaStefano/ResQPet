32e53fd89f8ed57e887a2b99ee1c6c37e4c1de17
giannn2 <g.delgaizo4@studenti.unisa.it>
2025-12-24 17:59:34 +0100

    Aggiunto il widget InfoMessage

---
+++ lib/widgets/info_message.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:resqpet/theme.dart';
+
+class InfoMessage extends StatelessWidget {
+
+  final String message;
+  final Color color;
+
+  const InfoMessage({
+    super.key,
+    required this.message,
+    this.color = ResQPetColors.primaryDark
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(40),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: color.withValues(alpha: 0.1),
+              shape: BoxShape.circle,
+            ),
+            child: Icon(Icons.pets, size: 60, color: color),
+          ),
+          const SizedBox(height: 20),
+          Text(
+            message,
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: color,
+            ),
+          ),
+        ],
+      ),
+    );
+    
+  }
+}
