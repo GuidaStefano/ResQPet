51969e6ebc0a46da10e022a1dad26f8bfa9000fe
Vittorio <grandevx@gmail.com>
2025-12-26 18:43:15 +0100

    Aggiunto UpdateAccountController

--- lib/repositories/utente_repository.dart
+++ lib/repositories/utente_repository.dart
@@ -178,8 +178,6 @@ class UtenteRepository {
     }
 
     await _utenteDao.deleteById(id);
-    await currentUser.delete();
-
     final callable = FirebaseFunctions.instance
       .httpsCallable("deleteUserAccountByUID");
 
